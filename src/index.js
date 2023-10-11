1b13d87804931d78aa5744e80d15349fa5e2a966
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2023-10-11 13:29:38 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const numToStr = n.toString();
+  const reversedStr = numToStr.split('').reverse().join('');
+  return parseInt(reversedStr, 10);
 }
